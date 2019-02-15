9b24e3c9c95a1c5ede3ed03105af73b38a92d282
sonic182 <johander1822@gmail.com>
2019-02-15 22:24:14 +0100

    First commit

---
+++ lib/links_fetcher.ex
@@ -0,0 +1,18 @@
+defmodule LinksFetcher do
+  @moduledoc """
+  Documentation for LinksFetcher.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LinksFetcher.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/links_fetcher_test.exs
@@ -0,0 +1,8 @@
+defmodule LinksFetcherTest do
+  use ExUnit.Case
+  doctest LinksFetcher
+
+  test "greets the world" do
+    assert LinksFetcher.hello() == :world
+  end
+end
