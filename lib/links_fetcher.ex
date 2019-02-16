a03bc2abb5e02da061f139ef1c468a0546a65d0d
sonic182 <johander1822@gmail.com>
2019-02-16 11:22:57 +0100

    Links fetcher with spawning process
    * With a check url fetched worker

--- lib/links_fetcher.ex
+++ lib/links_fetcher.ex
@@ -15,4 +15,102 @@ defmodule LinksFetcher do
   def hello do
     :world
   end
+
+  def fetch_links(url, depth \\ 1, fetched \\ [], statics \\ false) do
+    base = get_base(url)
+    {:ok, fetcher_checker} = Task.start_link(fn -> fetched([]) end)
+    parent = self()
+    spawn(fn -> do_fetch(url, base, depth, fetcher_checker, statics, parent) end)
+    Process.exit(fetcher_checker, :normal)
+    receive do
+      {:ok, links} ->
+        {:ok, links}
+    end
+  end
+
+  defp fetched(list) do
+    receive do
+      {:add, url} ->
+        fetched([url | list])
+      {:check, sender, url} ->
+        send(sender, {:check, Enum.member?(list, url)})
+    end
+    fetched(list)
+  end
+
+  defp do_fetch(url, base, depth, fetcher_checker, statics, caller) do
+    me = self()
+    send(fetcher_checker, {:check, me, url})
+
+    receive do
+      {:check, true} ->
+        send(caller, {:ok, []})
+      {:check, false} ->
+        if depth == 0 do
+          send(caller, {:ok, []})
+        else
+          {:ok, links} = fetch_data(url, statics)
+          send(fetcher_checker, {:add, url})
+          if Enum.empty?(links) do
+            send(caller, {:ok, []})
+          else
+            {:ok, newlinks} = Enum.map(links, fn x ->
+                spawn(fn ->
+                  do_fetch(base <> x, base, depth - 1, fetcher_checker, statics, me)
+                end)
+              end) |>
+              Enum.map(fn _x ->
+                receive do
+                  {:ok, links} -> {:ok, links}
+                end
+              end) |>
+              Enum.reduce(fn {:ok, links1}, {:ok, links2} -> {:ok, links1 ++ links2} end)
+
+            send(caller, {:ok, links ++ newlinks})
+          end
+        end
+    end
+  end
+
+  defp get_base(url) do
+    [_matched, base] = Regex.run(~r/(https?:\/\/[\w.]*)\/?/, url)
+    base
+  end
+
+  defp fetch_data(url, statics) do
+    case :hackney.request(
+      :get, url, [
+        pool: :default,
+        follow_redirect: true,
+        max_redirect: 5,
+        force_redirect: true
+      ]
+    ) do
+      {:ok, _status, _headers, client} ->
+        {:ok, body} = :hackney.body(client)
+        data = get_links(body, statics)
+        links = Enum.map(data, fn [_head | tail] -> tail end)
+          |> Enum.map(fn [item] -> item end)
+          |> Enum.reduce([], fn x, accum -> reduce_links(x, accum) end)
+        {:ok, links}
+      {:error, _error} ->
+        {:ok, []}
+    end
+  end
+
+  defp get_links(body, statics) do
+    if statics do
+      Regex.scan(~r/href="([\/]{1}[\w-.\/]*)"/, body)
+    else
+      Regex.scan(~r/href="([\/]{1}[\w-\/]*)"/, body)
+    end
+  end
+
+  defp reduce_links(link, accum) do
+    if Enum.member?(accum, link) do
+      accum
+    else
+      accum ++ [link]
+    end
+  end
 end
--- test/links_fetcher_test.exs
+++ test/links_fetcher_test.exs
@@ -2,7 +2,20 @@ defmodule LinksFetcherTest do
   use ExUnit.Case
   doctest LinksFetcher
 
-  test "greets the world" do
-    assert LinksFetcher.hello() == :world
+  test "minimal http sample" do
+    {:ok, _status, _respheaders, client} = :hackney.request(
+      :get,
+      "https://ipinfo.io/ip",
+      [pool: :default, follow_redirect: true, max_redirect: 5, force_redirect: true]
+    )
+    {:ok, body} = :hackney.body(client)
+    assert Regex.match?(~r/^[\d.]*\n$/, body)
+  end
+
+  test "fetch links" do
+    {:ok, links} = LinksFetcher.fetch_links("https://www.inmoduran.net/es/", 100)
+    all_are_links = Enum.map(links, fn x -> Regex.match?(~r/^[\/]{1}\S*$/, x) end) |>
+      Enum.reduce(true, fn x, y -> x and y end)
+    assert all_are_links
   end
 end
